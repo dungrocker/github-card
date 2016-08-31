c9bd9a4fa063723430c9288732b17a871cb56046
Dung <kiduyoona@gmail.com>
2016-08-31 14:39:08 +0700

    startProject

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
-
+$(document).ready(function () {
+   $("input").keypress(function (event) {
+       if (event.which == 13) {
+           var n = $("input").val();
+           $.get('https://api.github.com/users/' + n, function (data) {
+              var anh=data.avatar_url;
+              var ten=data.name;
+               $('.container').append('<div class="card"><img src="' + anh + ' " class="avatar"><p class="name">' + ten + '</p></div>');
+           });
+       }
+   });
+});
